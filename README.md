6993fb8a7ca74277b9812180ce7943807acf545b
Sanpi <sanpi@homecomputing.fr>
2016-01-14 14:48:21 +0100

    User defined protocole

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ bower install onionify.js
 # Usage
 
 ```html
-<script src="bower_components/onionify.js/src/onionify.js" data-onion="dua6u3dsufohrnsz.onion"></script>
+<script src="bower_components/onionify.js/src/onionify.js" data-onion="https://dua6u3dsufohrnsz.onion"></script>
 ```
 
 The script should be loaded **synchronously**. No *async* attribute.
--- src/onionify.js
+++ src/onionify.js
@@ -15,15 +15,14 @@ var onion = getData().onion;
 if (onion !== null) {
     if (window.location.protocol + "//" + window.location.hostname != onion) {
         var query = new XMLHttpRequest();
-        var url = window.location.protocol + "//" + onion;
 
         query.onload = function(e) {
             if (query.status === 200) {
-                window.location.assign(url + window.location.pathname);
+                window.location.assign(onion + window.location.pathname);
             }
         };
         query.timeout = 5000;
-        query.open('HEAD', url, true);
+        query.open('HEAD', onion, true);
         query.send();
     }
 }
