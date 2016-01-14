05e53cf07fb7bbe64e81ff5f6f11ea619b258e58
Sanpi <sanpi@homecomputing.fr>
2016-01-14 13:37:10 +0100

    Check onion parameter

--- src/onionify.js
+++ src/onionify.js
@@ -10,15 +10,20 @@ function getData()
 
 var onion = getData().domain;
 
-if (window.location.protocol + "//" + window.location.hostname != onion) {
-    var query = new XMLHttpRequest();
+if (onion !== null) {
+    if (window.location.protocol + "//" + window.location.hostname != onion) {
+        var query = new XMLHttpRequest();
 
-    query.onload = function(e) {
-        if (query.status == 200) {
-            window.location.assign(onion + window.location.pathname);
-        }
-    };
-    query.timeout = 5000;
-    query.open('HEAD', onion, true);
-    query.send();
-};
+        query.onload = function(e) {
+            if (query.status === 200) {
+                window.location.assign(onion + window.location.pathname);
+            }
+        };
+        query.timeout = 5000;
+        query.open('HEAD', onion, true);
+        query.send();
+    }
+}
+else {
+    console.error("Missing data-domain attribute. Ensure that script loaded synchronously.");
+}
